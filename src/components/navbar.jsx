ad899352cfec739ad9786ce592bf243bc5cba018
Rebeca Heredia <rbkheredia90@gmail.com>
2023-07-09 20:49:38 -0300

    responsive home

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,15 +1,23 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { GlobalContext } from "../provider/provider";
 import logo from '/assets/shared/logo.svg';
+import burger from '/assets/shared/icon-hamburger.svg';
 import { Link } from "react-router-dom";
 import '../styles/components/navbar.scss';
 
 export default function Navbar(){
+  const [openNav, setOpenNav] = useState(false);
+
+  const handleNav = () =>{
+    setOpenNav(!openNav)
+  }
   return(
     <header className="header-navbar">
       <Logo />
+      
       <Line />
-      <Nav />
+      <Nav openNav={openNav}/>
+      <IconBurger handleNav={handleNav}/>
     </header>
   )
 }
@@ -20,17 +28,26 @@ const Logo = () =>{
   )
 }
 
+const IconBurger = ({ handleNav }) =>{
+  return(
+    <button className="burger" onClick={handleNav}>
+      <img src={burger} />
+    </button>
+    
+  )
+}
+
 const Line = () => {
   return(
     <div className="line-nav"></div>
   )
 }
 
-const Nav = () => {
+const Nav = ({ openNav }) => {
   const { header } = useContext(GlobalContext);
 
   return(
-    <nav>
+    <nav className={` ${openNav && 'active'}` }>
       <ul className="list-nav">
         {header.map ((li) =>
           <Link key={li.id} to={li.href} style={{color:'white', textDecoration:'none'}}>{li.id}&nbsp; {li.title}</Link>
